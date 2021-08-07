1d9c254676b4399570583a2b6bb84e17b2914c6e
Samuel Hawker <samuel.hawker@ibm.com>
2021-08-07 18:44:27 +0100

    feat: Shading sphere
    Signed-off-by: Samuel Hawker <samuel.hawker@ibm.com>

--- src/main.rs
+++ src/main.rs
@@ -214,21 +214,28 @@ impl Ray {
 }
 impl Ray {
     fn ray_color(&self) -> color {
-        if self.hit_sphere(point3::new(0.0,0.0,-1.0), 0.5) {
-            return color::new(1.0, 0.0, 0.0)
+        let t = self.hit_sphere(point3::new(0.0, 0.0, -1.0), 0.5);
+        if t > 0.0 {
+            let n = (self.at(t) - vec3::new(0.0, 0.0, -1.0)).unit_vector();
+            return color::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0) * 0.5;
         }
+
         let unit_direction = self.direction().unit_vector();
+        // shadowing
         let t = 0.5 * (unit_direction.y() + 1.0);
         color::new(1.0, 1.0, 1.0).multiply(1.0 - t) + color::new(0.5, 0.7, 1.0).multiply(t)
     }
 
-    fn hit_sphere(&self, center: point3, radius: f64) -> bool {
+    fn hit_sphere(&self, center: point3, radius: f64) -> f64 {
         let oc = self.origin() - center;
         let dir = self.direction();
-        let a = self.direction().dot(&dir);
-        let b = 2.0 * oc.dot(&dir);
-        let c = oc.dot(&oc) - radius*radius;
-        let discriminant = b*b - a*c*4.0;
-        discriminant > 0.0
+        let a = self.direction().length_squared();
+        let half_b = oc.dot(&dir);
+        let c = oc.length_squared() - radius * radius;
+        let discriminant = half_b * half_b - a * c;
+        if discriminant < 0.0 {
+            return -1.0;
+        }
+        (-half_b - discriminant.sqrt()) / a
     }
 }
